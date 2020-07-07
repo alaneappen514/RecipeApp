501acb41f676df7f0806ef16e878544415f2ffbc
alaneappen514 <eappenalan123@gmail.com>
2020-07-07 12:14:18 -0400

    styled login page removed navbar on new and edit routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :recipes 
+
+  def username
+     return self.email.split('@')[0].capitalize
+  end
 end
